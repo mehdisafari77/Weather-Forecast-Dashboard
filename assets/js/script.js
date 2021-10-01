85d2963ec38b156842d3cbdd65d667f4ec309f5c
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-01 15:10:18 -0700

    first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,20 @@
-$("search-button").on("click", function(){
+$("#search-button").on("click", function() {
     var searchValue = $("#search-value").val()
 
     console.log(searchValue)
-
-
+    getCurrentWeather(searchValue)
 })
+
+
+
+function getCurrentWeather(searchValue) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=d91f911bcf2c0f925fb6535547a5ddc9"
+
+    fetch(apiUrl).then(function(response) {
+        return response.json()
+    })
+    .then(function(response) {
+        console.log(response)
+    })
+
+}
